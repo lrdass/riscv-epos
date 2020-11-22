6cb3fe0fdfac079fbdc950950d20884ef241750c
lrdass <lsuppes@pm.me>
2020-11-22 19:44:00 -0300

    manycore working?

--- src/api/system_init.cc
+++ src/api/system_init.cc
@@ -8,10 +8,8 @@ __BEGIN_SYS
 
 void System::init()
 {
-    if(CPU::id() == 0) {
-        if(Traits<Alarm>::enabled)
-            Alarm::init();
-    }
+    if(Traits<Alarm>::enabled)
+        Alarm::init();
 
     if(Traits<Thread>::enabled)
         Thread::init();
